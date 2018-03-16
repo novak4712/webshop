cb56366037e99b58b523f56a21629cf71dedcf8b
novak4712 <novak4803@gmail.com>
2018-03-16 21:26:32 +0300

    asdasd

---
+++ resources/views/front/cart.blade.php
@@ -0,0 +1,14 @@
+
+
+<ul class="list-group">
+    @foreach($products as $product)
+
+
+        <li class="list-group-item">
+            {{ $product['item'] ['name'] }}
+            {{ $product['price'] }}
+            <span class="badge">{{ $product['qty'] }}</span>
+        </li>
+
+    @endforeach
+</ul>
